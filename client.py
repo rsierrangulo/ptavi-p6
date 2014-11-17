83cb283d82bc8d584e79838e7a0965b67beef49d
alba garcia de la camacha selgas <albagc@iota32.aulas.gsyc.es>
2014-11-17 18:32:26 +0100

    Acabado client.py

--- client.py
+++ client.py
@@ -5,28 +5,43 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
+if len(sys.argv) != 3:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+
+METHOD = sys.argv[1]
+
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+SERVER = sys.argv[2].split("@")[1].split(":")[0]
+PORT = int(sys.argv[2].split(":")[1])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = sys.argv[2].split(":")[0]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
-
+print "Enviando: " + METHOD + " sip:" + LINE + " SIP/2.0\r\n\r\n"
+my_socket.send(METHOD + " sip:" + LINE + " SIP/2.0\r\n\r\n")
+#En caso de que nuestro servidor no haya sido lanzado 
+try:
+    data = my_socket.recv(1024)
+except socket.error:
+    sys.exit("Error: No server listening at " + SERVER + " PORT " + str(PORT))
 print 'Recibido -- ', data
-print "Terminando socket..."
 
+if METHOD == 'INVITE':
+    #He recibido los tres mensajes a la vez, en una misma línea
+    METHOD = 'ACK'
+    print "Enviando: " + METHOD + " sip:" + LINE + " SIP/2.0\r\n\r\n"
+    my_socket.send(METHOD + " sip:" + LINE + " SIP/2.0\r\n\r\n")
+
+print "Terminando socket..."
 # Cerramos todo
 my_socket.close()
 print "Fin."
