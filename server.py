3ea67c4dd55d298e6c461fb5ca01dcc8f6a3d82e
raquel sierra angulo <rsierra@iota18.aulas.gsyc.es>
2014-11-17 17:16:39 +0100

    server 2

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import SocketServer
 import sys
+import os
 
 comandos = sys.argv
 
@@ -24,35 +25,30 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         while 1:
             line = self.rfile.read()
             print "El cliente nos manda " + line
-            lista = line.split()
+            lista = line.split(" ")
             metodo = lista[0]
             metodos = ['INVITE', 'ACK', 'BYE']
-            if metodo == INVITE
+
+            if not line:
+                break
+
+            if metodo == "INVITE":
                 respuesta = "SIP/2.0 100 Trying\r\n\r\n"
                 respuesta += "SIP/2.0 180 Ring\r\n\r\n"
                 respuesta += "SIP/2.0 200 OK\r\n\r\n"
                 self.wfile.write(respuesta)
-            elif metodo == ACK
-                print "recibido ACK"
-            elif metodo == BYE
+            elif metodo == "ACK":
+                aEjecutar = './mp32rtp -i 127.0.0.1 -p 230321 <' + FICH_AUDIO
+                os.system('chmod 755 mp32rtp')                
+                os.system(aEjecutar)
+            elif metodo == "BYE":
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
             elif not metodo in metodos:
                 self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
             else:
-                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
-                
-                
-                
-                
-                
-
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+                self.wfile.write("SIP/2.0 400 Bad\r\n\r\n")
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
     if len(comandos) != 4:
         print "Usage: python server.py IP port audio_file"
     serv = SocketServer.UDPServer((SERVER, PORT), EchoHandler)
