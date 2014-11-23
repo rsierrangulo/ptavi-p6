01edd0b53397dcf08585f5d63208865200028a80
nmalo5 <nmszidane@msn.com>
2014-11-23 13:34:27 +0000

    envios y mensajes

--- client.py
+++ client.py
@@ -16,32 +16,33 @@ metodo = sys.argv[1]
 direccion = sys.argv[2].split("@")
 	# Dirección IP y puerto del servidor.
 LOGIN = direccion[0]
-#print LOGIN 
 IPPORT = direccion[1].split(":")
-SERVER = IPPORT[0]
+SERVERIP = IPPORT[0]
 SIPPORT = int(IPPORT[1])
-#print IPPORT[0]
-#print IPPORT[1]
+
 	# Contenido que vamos a enviar
-#try: 
+
 	# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, SIPPORT))
-#except SocketError 
-    #print "Error: No server listening at 193.147.73.20 port 5555" 
-
-print "Enviando: " + metodo
-my_socket.send(metodo + '\r\n')
-data = my_socket.recv(1024)
-print 'Recibido -- ', data
-if data ==  "SIP/2.0 100 Trying \r\n SIP/2.0 180 Ring \r\n SIP/2.0 200 OK":
-    metodo = "ACK"
-    print "Enviando: " + metodo
-    my_socket.send(metodo + '\r\n')
-    
+my_socket.connect((SERVERIP, SIPPORT))
+#REGISTER sip:luke@polismassa.com SIP/2.0\r\n\r\n
+line = " sip:" + LOGIN + "@" + "SIP /2.0\r\n\r\n"
+envio = metodo + line
+print "Enviando: " + envio
+my_socket.send(envio)
+try: 
+    data = my_socket.recv(1024)
+    print 'Recibido -- ', data
+    if data ==  "SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n":
+        metodo = "ACK"
+        envio = metodo + line
+        print "Enviando: " + envio 
+        my_socket.send(envio)
+except socket.error:
+    print "Error: No server listening at " + SERVERIP + " port " + str(SIPPORT)   
 #print "Terminando socket..."
 
 # Cerramos todo
-#my_socket.close()
+my_socket.close()
 #print "Fin."
