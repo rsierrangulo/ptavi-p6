2d74044aeed8c73c4fcbce91c783f72013c9daf1
nmalo5 <nmszidane@msn.com>
2014-11-23 15:47:38 +0000

    Vprov y pep8

--- server.py
+++ server.py
@@ -6,7 +6,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import SocketServer
 import sys
-import os 
+import os
+
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -15,7 +16,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             #SIP/2.0 200 OK\r\n\r\n
@@ -26,34 +26,34 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             # Si no hay más líneas salimos del bucle infinito
             if line[0] == "INVITE":
                 print "INVITE recibido"
-                self.wfile.write("SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n")
-                
+                self.wfile.write("SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring"
+                                 "\r\n\r\nSIP/2.0 200 OK\r\n\r\n")
+
             elif line[0] == "ACK":
-                print "ACK recibido" 
-                # aEjecutar es un string con lo que se ha de ejecutar en la shell 
-                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 <' + fichero_audio 
-                print "Vamos a ejecutar", aEjecutar 
-                os.system(aEjecutar) 
+                print "ACK recibido"
+            # aEjecutar es un string con lo que se ha de ejecutar en la shell
+                aEjecutar = " mp32rtp -i 127.0.0.1 -p 23032 < " + fichero_audio
+                print "Vamos a ejecutar", aEjecutar
+                os.system(aEjecutar)
 
             elif line[0] == "BYE":
                 print "BYE recibido"
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-            elif line[0] == "CANCEL" or line[0] == "REGISTER" or line[0] == "OPTIONS":
+            elif (line[0] == "CANCEL" or line[0] == "REGISTER"
+                  or line[0] == "OPTIONS"):
                 print "metodo no disponible recibido"
-                self.wfile.write("SIP/2.0 Method Not Allowed\r\n\r\n") 
+                self.wfile.write("SIP/2.0 Method Not Allowed\r\n\r\n")
             else:
                 print "petición incorrecta recibida"
-                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n") 
-            
-            
+                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-	    sys.exit("Usage: python server.py IP port audio_file")
+        sys.exit("Usage: python server.py IP port audio_file")
     else:
         print "Listening..."
     # Creamos servidor de eco y escuchamos
-    fichero_audio = sys.argv[3] 
+    fichero_audio = sys.argv[3]
     PORT = int(sys.argv[2])
     serv = SocketServer.UDPServer(("", PORT), EchoHandler)
     print "Lanzando servidor UDP de eco..."
