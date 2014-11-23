02a18e24b78ece397735cad4c3023aecb37ca913
nmalo5 <nmszidane@msn.com>
2014-11-23 15:47:58 +0000

    Vprov y pep8

--- client.py
+++ client.py
@@ -7,40 +7,40 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-	#Cliente SIP
+    #Cliente SIP
 
 if len(sys.argv) != 3:
-	sys.exit("Usage: python client.py method receiver@IP:SIPport")
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
 metodo = sys.argv[1]
 direccion = sys.argv[2].split("@")
-	# Dirección IP y puerto del servidor.
+    # Dirección IP y puerto del servidor.
 LOGIN = direccion[0]
 IPPORT = direccion[1].split(":")
 SERVERIP = IPPORT[0]
 SIPPORT = int(IPPORT[1])
 
-	# Contenido que vamos a enviar
+    # Contenido que vamos a enviar
 
-	# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVERIP, SIPPORT))
-#REGISTER sip:luke@polismassa.com SIP/2.0\r\n\r\n
-line = " sip:" + LOGIN + "@" + "SIP /2.0\r\n\r\n"
+
+line = " sip:" + LOGIN + "@" + "SIP/2.0\r\n\r\n"
 envio = metodo + line
 print "Enviando: " + envio
 my_socket.send(envio)
-try: 
+try:
     data = my_socket.recv(1024)
     print 'Recibido -- ', data
-    if data ==  "SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n":
+    if data == "SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n":
         metodo = "ACK"
         envio = metodo + line
-        print "Enviando: " + envio 
+        print "Enviando: " + envio
         my_socket.send(envio)
 except socket.error:
-    print "Error: No server listening at " + SERVERIP + " port " + str(SIPPORT)   
+    print "Error: No server listening at " + SERVERIP + " port " + str(SIPPORT)
 #print "Terminando socket..."
 
 # Cerramos todo
